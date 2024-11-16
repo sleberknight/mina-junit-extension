5738a3d2ae1510d64e5a961bbe7e0867be21141c
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-11-15 20:52:46 -0500

    Log instead of using sysout

--- src/main/java/com/acme/example/mina/TimeServerHandler.java
+++ src/main/java/com/acme/example/mina/TimeServerHandler.java
@@ -1,33 +1,41 @@
 package com.acme.example.mina;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 
-import java.util.Date;
+import java.time.Instant;
 
+@Slf4j
 public class TimeServerHandler extends IoHandlerAdapter {
 
     @Override
-    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
-        cause.printStackTrace();
+    public void exceptionCaught(IoSession session, Throwable cause) {
+        LOG.error("TimeServerHandler caught exception for session {}", session.getId(), cause);
     }
 
     @Override
-    public void messageReceived(IoSession session, Object message) throws Exception {
-        String str = message.toString();
-        if (str.trim().equalsIgnoreCase("quit")) {
-            session.close();
+    public void messageReceived(IoSession session, Object message) {
+        var sessionId = session.getId();
+        LOG.info("Session {} received a message", sessionId);
+
+        var str = message.toString();
+        if (str.strip().equalsIgnoreCase("quit")) {
+            LOG.info("quit received for session {}", sessionId);
+            session.closeNow();
             return;
         }
 
-        Date date = new Date();
-        session.write(date.toString());
-        System.out.println("Message written...");
+        LOG.info("Session {}: You said: {}", sessionId, str);
+
+        var now = Instant.now();
+        session.write("The time is now " + now.toString());
+        LOG.info("Session {}: Message written...", sessionId);
     }
 
     @Override
-    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
-        System.out.println("IDLE " + session.getIdleCount(status));
+    public void sessionIdle(IoSession session, IdleStatus status) {
+        LOG.info("session {} is idle", session.getIdleCount(status));
     }
 }
