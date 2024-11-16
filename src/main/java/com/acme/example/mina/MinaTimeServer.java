5ee1478d26fe4ddda6239a1e5df8693757a427a9
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-11-15 20:51:59 -0500

    Sonar: use UTF_8 constant

--- src/main/java/com/acme/example/mina/MinaTimeServer.java
+++ src/main/java/com/acme/example/mina/MinaTimeServer.java
@@ -9,7 +9,7 @@ import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class MinaTimeServer {
 
@@ -20,7 +20,7 @@ public class MinaTimeServer {
 
         acceptor.getFilterChain().addLast("logger", new LoggingFilter());
         acceptor.getFilterChain().addLast("codec",
-                new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+                new ProtocolCodecFilter(new TextLineCodecFactory(StandardCharsets.UTF_8)));
 
         acceptor.setHandler(new TimeServerHandler());
 
