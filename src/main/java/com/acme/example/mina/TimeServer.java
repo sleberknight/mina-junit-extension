de92dd3d8916495eb99769b2898fa8e0b5bd2c88
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-11-17 14:01:14 -0500

    TimeServer can now be stopped and can provide recently received messages

--- src/main/java/com/acme/example/mina/TimeServer.java
+++ src/main/java/com/acme/example/mina/TimeServer.java
@@ -8,21 +8,54 @@ import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TimeServer {
-    public void createAndStart(int port) throws IOException {
-        IoAcceptor acceptor = new NioSocketAcceptor();
+
+    private final AtomicBoolean started;
+    private IoAcceptor acceptor;
+    private TimeServerHandler handler;
+
+    public TimeServer() {
+        started = new AtomicBoolean();
+    }
+
+    public void createAndStart(int port) {
+        if (started.get()) {
+            throw new IllegalStateException("Already started");
+        }
+
+        acceptor = new NioSocketAcceptor();
+        handler = new TimeServerHandler();
 
         acceptor.getFilterChain().addLast("logger", new LoggingFilter());
         acceptor.getFilterChain().addLast("codec",
                 new ProtocolCodecFilter(new TextLineCodecFactory(StandardCharsets.UTF_8)));
 
-        acceptor.setHandler(new TimeServerHandler());
+        acceptor.setHandler(handler);
 
         acceptor.getSessionConfig().setReadBufferSize(2048);
         acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);  // seconds
-        acceptor.bind(new InetSocketAddress(port));
+        try {
+            acceptor.bind(new InetSocketAddress(port));
+        } catch (IOException e) {
+            throw new UncheckedIOException("I/O error binding to port " + port, e);
+        }
+
+        started.set(true);
+    }
+
+    public void stopNow() {
+        if (started.get()) {
+            acceptor.unbind();
+        }
+    }
+
+    public List<String> recentMessages() {
+        return handler.recentMessages();
     }
 }
